991711809af4dde6343e11c08347c5c8461dc211
dongzhengshang <dzsdevelop@163.com>
2016-12-08 10:27:14 +0800

    修复Toast BUG

--- projectframe/src/main/java/com/dzs/projectframe/utils/TostUtils.java
+++ projectframe/src/main/java/com/dzs/projectframe/utils/TostUtils.java
@@ -16,31 +16,27 @@ import com.dzs.projectframe.base.ProjectContext;
 public class TostUtils {
     private static String oldMsg;
     private static Toast toast = null;
-    private static long oneTime = 0;
-    private static long twoTime = 0;
+    private static long lastTime = 0;
 
     /**
      * 多次触发只显示一次Toast
      */
-    public static void showOneToast(Context context, String str, Toast t) {
-        if (StringUtils.isEmpty(str)) {
-            return;
-        }
+    public static void showOneToast(Context context, String str) {
+        if (StringUtils.isEmpty(str)) return;
         if (toast == null) {
-            oneTime = System.currentTimeMillis();
-            toast = t == null ? createToast(context, str) : t;
+            toast = createToast(context, str);
+            lastTime = System.currentTimeMillis();
+            toast.show();
         } else {
-            twoTime = System.currentTimeMillis();
             if (str.equals(oldMsg)) {
-                if (twoTime - oneTime > Toast.LENGTH_SHORT) toast.show();
+                if (System.currentTimeMillis() - lastTime > Toast.LENGTH_LONG) toast.show();
             } else {
                 toast.setText(str);
                 toast.show();
             }
+            lastTime = System.currentTimeMillis();
         }
         oldMsg = str;
-        oneTime = twoTime;
-
     }
 
     /**
@@ -49,11 +45,11 @@ public class TostUtils {
      * @param strId ID
      */
     public static void showOneToast(int strId) {
-        showOneToast(ProjectContext.appContext, ProjectContext.appContext.getString(strId), null);
+        showOneToast(ProjectContext.appContext, ProjectContext.appContext.getString(strId));
     }
 
     public static void showOneToast(String str) {
-        showOneToast(ProjectContext.appContext, str, null);
+        showOneToast(ProjectContext.appContext, str);
     }
 
     /**
@@ -63,6 +59,10 @@ public class TostUtils {
      */
     private static Toast createToast(Context context, String str) {
         return Toast.makeText(context, str, Toast.LENGTH_SHORT);
+    }
+
+    public static void setToast(Toast t) {
+        toast = t;
     }
 
     /**
