baaa26e2e742a71ec627ff5b5f2ec126a70106ea
dongzhengshang <dzsdevelop@163.com>
2016-12-22 11:02:27 +0800

    更改AES加密

--- projectframe/src/main/java/com/dzs/projectframe/utils/EncryptionUtils.java
+++ projectframe/src/main/java/com/dzs/projectframe/utils/EncryptionUtils.java
@@ -19,6 +19,7 @@ public class EncryptionUtils {
     private static final String CIPHER_ALGORITHM_CBC = "AES/CBC/PKCS7Padding";
     private static final String UTF_8 = "UTF-8";
     private static final String MD5 = "MD5";
+    private static final String AES = "AES";
 
     /**
      * MD5 32位加密
@@ -54,32 +55,25 @@ public class EncryptionUtils {
                 result = sb.toString();// 32位加密
             }
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            LogUtils.exception(e);
         }
         return result;
     }
 
     //创建AES密钥
-    private static SecretKeySpec createAESKey(String password) {
+    public static SecretKeySpec createAESKey(String password) {
         byte[] data = null;
-        if (password == null) {
-            password = "";
-        }
+        if (password == null) password = "";
         StringBuilder sb = new StringBuilder(32);
         sb.append(password);
-        while (sb.length() < 32) {
-            sb.append("0");
-        }
-        if (sb.length() > 32) {
-            sb.setLength(32);
-        }
-
+        while (sb.length() < 32) sb.append("0");
+        if (sb.length() > 32) sb.setLength(32);
         try {
             data = sb.toString().getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            LogUtils.exception(e);
         }
-        return new SecretKeySpec(data, "AES");
+        return new SecretKeySpec(data, AES);
     }
 
     public static byte[] encrypt(byte[] content, String key, String iv) {
@@ -92,12 +86,12 @@ public class EncryptionUtils {
                 plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
             }
             byte[] plaintext = new byte[plaintextLength];
-            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), AES);
             IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
             cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
             return cipher.doFinal(content);
         } catch (Exception e) {
-            e.printStackTrace();
+            LogUtils.exception(e);
         }
         return null;
     }
@@ -107,7 +101,7 @@ public class EncryptionUtils {
         byte[] data = null;
         try {
             data = content.getBytes("UTF-8");
-        } catch (Exception e) {
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         data = encrypt(data, key, iv);
@@ -139,56 +133,44 @@ public class EncryptionUtils {
     public static byte[] decrypt(byte[] content, String key, String iv) {
         try {
             Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM_CBC);
-            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), AES);
             IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
-            byte[] result = cipher.doFinal(content);
-            return result;
+            return cipher.doFinal(content);
         } catch (Exception e) {
-            e.printStackTrace();
+            LogUtils.exception(e);
         }
         return null;
     }
 
+    //16进制字符串转换为二进制数组
+    public static byte[] hex2byte(String inputString) {
+        if (inputString == null || inputString.length() < 2) {
+            return new byte[0];
+        }
+        return hex2byte(inputString.getBytes());
+    }
+
     //16进制转换为二进制
-    private static byte[] hex2byte(byte[] b) {
-        if ((b.length % 2) != 0)
-            throw new IllegalArgumentException();
+    public static byte[] hex2byte(byte[] b) {
+        if ((b.length % 2) != 0) throw new IllegalArgumentException();
         byte[] b2 = new byte[b.length / 2];
         for (int n = 0; n < b.length; n += 2) {
             String item = new String(b, n, 2);
-            b2[n / 2] = (byte) Integer.parseInt(item, 16);
+            b2[n / 2] = (byte) (Integer.parseInt(item, 16) & 0xFF);
         }
         return b2;
     }
 
     //二进制转换为16进制字符串
-    private static String byte2hex(byte[] b) {
-        StringBuffer sb = new StringBuffer(b.length * 2);
+    public static String byte2hex(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
         String tmp;
         for (byte aB : b) {
             tmp = (Integer.toHexString(aB & 0XFF));
-            if (tmp.length() == 1) {
-                sb.append("0");
-            }
+            if (tmp.length() == 1) sb.append("0");
             sb.append(tmp);
         }
         return sb.toString().toUpperCase();
     }
-
-    //16进制字符串转换为二进制数组
-    private static byte[] hex2byte(String inputString) {
-        if (inputString == null || inputString.length() < 2) {
-            return new byte[0];
-        }
-        inputString = inputString.toLowerCase();
-        int l = inputString.length() / 2;
-        byte[] result = new byte[l];
-        for (int i = 0; i < l; ++i) {
-            String tmp = inputString.substring(2 * i, 2 * i + 2);
-            result[i] = (byte) (Integer.parseInt(tmp, 16) & 0xFF);
-        }
-        return result;
-    }
-
 }
