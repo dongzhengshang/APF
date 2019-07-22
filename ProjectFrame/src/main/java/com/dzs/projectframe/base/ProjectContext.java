bc40b2a7bd56f834ccf5acbf0e13f3950e78726b
DZS <dongzhengshang@zhidekan.me>
2019-07-22 18:08:37 +0800

    删除无用数据

--- ProjectFrame/src/main/java/com/dzs/projectframe/base/ProjectContext.java
+++ ProjectFrame/src/main/java/com/dzs/projectframe/base/ProjectContext.java
@@ -17,80 +17,79 @@ import com.dzs.projectframe.utils.SystemUtils;
  * @date 2016/8/29.
  */
 public class ProjectContext extends Application {
-    public static ProjectContext appContext;
-    public static Resources resources;
-    public static SharedPreferUtils sharedPreferUtils;
-    private Receiver receiver;
-    public boolean isH5Refresh = false;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        appContext = this;
-        resources = appContext.getResources();
-        sharedPreferUtils = SharedPreferUtils.getInstanse(this);
-        setBroadcast();
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        resources = null;
-        receiver.clear();
-        unregisterReceiver(receiver);
-        sharedPreferUtils.close();
-    }
-
-    /**
-     * 设置广播
-     */
-    private void setBroadcast() {
-        receiver = new Receiver();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Receiver.ACTION);
-        registerReceiver(receiver, intentFilter);
-    }
-
-    /**
-     * 发送广播
-     *
-     * @param netEntity netEntity
-     */
-    public void sendBroadcast(NetEntity netEntity) {
-        Intent intent = new Intent(Receiver.ACTION);
-        intent.putExtra(NetEntity.class.getName(), netEntity);
-        sendBroadcast(intent);
-    }
-
-    /*添加广播监听*/
-    public void addReceiver(Receiver.OnBroadcastReceiverListener receiverListener) {
-        receiver.addReceiver(receiverListener);
-    }
-
-    /*移除广播监听*/
-    public void removeReceiver(Receiver.OnBroadcastReceiverListener receiverListener) {
-        receiver.removeReceiver(receiverListener);
-    }
-
-    /**
-     * 获取当前应用程序版本号
-     *
-     * @return int
-     */
-    public int getApplicationVersion() {
-        PackageInfo info = SystemUtils.getPackageInfo(this);
-        return info.versionCode;
-    }
-
-    /**
-     * 获取应用程序版本名称
-     *
-     * @return String
-     */
-    public String getApplicationVersionName() {
-        PackageInfo info = SystemUtils.getPackageInfo(this);
-        return info.versionName;
-    }
-
-
+	public static ProjectContext appContext;
+	public static Resources resources;
+	public static SharedPreferUtils sharedPreferUtils;
+	private Receiver receiver;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		appContext = this;
+		resources = appContext.getResources();
+		sharedPreferUtils = SharedPreferUtils.getInstanse(this);
+		setBroadcast();
+	}
+	
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+		resources = null;
+		receiver.clear();
+		unregisterReceiver(receiver);
+		sharedPreferUtils.close();
+	}
+	
+	/**
+	 * 设置广播
+	 */
+	private void setBroadcast() {
+		receiver = new Receiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(Receiver.ACTION);
+		registerReceiver(receiver, intentFilter);
+	}
+	
+	/**
+	 * 发送广播
+	 *
+	 * @param netEntity netEntity
+	 */
+	public void sendBroadcast(NetEntity netEntity) {
+		Intent intent = new Intent(Receiver.ACTION);
+		intent.putExtra(NetEntity.class.getName(), netEntity);
+		sendBroadcast(intent);
+	}
+	
+	/*添加广播监听*/
+	public void addReceiver(Receiver.OnBroadcastReceiverListener receiverListener) {
+		receiver.addReceiver(receiverListener);
+	}
+	
+	/*移除广播监听*/
+	public void removeReceiver(Receiver.OnBroadcastReceiverListener receiverListener) {
+		receiver.removeReceiver(receiverListener);
+	}
+	
+	/**
+	 * 获取当前应用程序版本号
+	 *
+	 * @return int
+	 */
+	public int getApplicationVersion() {
+		PackageInfo info = SystemUtils.getPackageInfo(this);
+		return info.versionCode;
+	}
+	
+	/**
+	 * 获取应用程序版本名称
+	 *
+	 * @return String
+	 */
+	public String getApplicationVersionName() {
+		PackageInfo info = SystemUtils.getPackageInfo(this);
+		return info.versionName;
+	}
+	
+	
 }
